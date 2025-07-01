ee59f2ad1824a4310ba9d21344cfae308399fb08
Ashley Daniel <ashleyb@Ashleys-MBP.attlocal.net>
2025-07-01 14:02:23 -0500

    Force refresh route.ts to fix stale params error

--- schemaTypes/index.ts
+++
@@ -1,9 +0,0 @@
-import vibematchBot from './vibematch'
-import lightprompted from './lightprompted'
-import bot from './bot'
-import product from './product'
-import courseModule from './courseModule'
-
-export const schemaTypes = [bot, vibematchBot, lightprompted, product, courseModule]
-
-export default schemaTypes
